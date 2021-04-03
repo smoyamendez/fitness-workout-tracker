77ee33480e9a4f016a13ae6e548e694b672bf2ae
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-04-03 13:57:23 -0400

    fix seed issue for heroku

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,9 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect((process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 
 let workoutSeed = [
